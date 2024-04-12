426d6cbc81b4183f1a170920fb93ae154bd10cbc
JuhyunPark <epahs1831@naver.com>
2024-04-12 19:57:30 +0900

    Init

---
+++ src/main/java/com/sideProject/dribbleMatch/DribbleMatchApplication.java
@@ -0,0 +1,13 @@
+package com.sideProject.dribbleMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DribbleMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DribbleMatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideProject/dribbleMatch/DribbleMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideProject.dribbleMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DribbleMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
