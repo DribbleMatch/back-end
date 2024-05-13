977f254dae2caaebc0be23deecced20777ca96d4
moonjin-kim <ace39418@gmail.com>
2024-05-13 23:44:35 +0900

    Test: Recruitment 조회 테스트 추가 #40

--- src/test/java/com/sideProject/DribbleMatch/repository/recruitment/RecruitmentIntegrationRepositoryTest.java
+++ src/test/java/com/sideProject/DribbleMatch/repository/recruitment/RecruitmentIntegrationRepositoryTest.java
@@ -49,9 +49,9 @@ public class RecruitmentIntegrationRepositoryTest {
         regionRepository.deleteAllInBatch();
     }
 
-    private Region initRegion(String dong) {
+    private Region initRegion(String sido,String dong) {
         return regionRepository.save(Region.builder()
-                .siDo("서울특별시")
+                .siDo(sido)
                 .siGunGu("영등포구")
                 .eupMyeonDongGu(dong)
                 .latitude(37.5347)
@@ -97,11 +97,11 @@ public class RecruitmentIntegrationRepositoryTest {
     @DisplayName("RecruitmentFindTest(통합)")
     class Find{
 
-        @DisplayName("")
+        @DisplayName("모집글을 조회할 수 있다.")
         @Test
         void recruitmentFindTest(){
             //given
-            Region region = initRegion("당산동");
+            Region region = initRegion("서울특별시","당산동");
             User leader = initUser("tes1t@test.com","test", region);
             Team team = initTeam("testTeam", leader, region);
             Recruitment recruitment = initRecruitment("테스트 모집글", team);
@@ -120,6 +120,37 @@ public class RecruitmentIntegrationRepositoryTest {
             assertThat(result.getContent().get(0).getContent()).isEqualTo(recruitment.getContent());
             assertThat(result.getContent().get(0).getTitle()).isEqualTo(recruitment.getTitle());
             assertThat(result.getContent().get(0).getTeam().getId()).isEqualTo(recruitment.getTeam().getId());
+        }
+
+        @DisplayName("모집글을 지역명으로 조회할 수 있다.")
+        @Test
+        void recruitmentFindTestByRegion(){
+            //given
+            Region region1 = initRegion("서울특별시","당산동");
+            User leader1 = initUser("tes1t@test.com","test", region1);
+            Team team1 = initTeam("testTeam", leader1, region1);
+
+            Region region2 = initRegion("울산광역시","당산동");
+            User leader2 = initUser("test2@test.com","test2", region2);
+            Team team2 = initTeam("testTeam2", leader2, region2);
+
+            Recruitment recruitment1 = initRecruitment("테스트 모집글", team1);
+            Recruitment recruitment2 = initRecruitment("테스트 모집글", team2);
+
+            //when
+            Pageable pageable = PageRequest.of(0, 2);
+            RecruitmentSearchParamRequest param = new RecruitmentSearchParamRequest(
+                    "서울특별시",
+                    null
+            );
+
+            Page<Recruitment> result = recruitmentRepository.find(pageable,param);
+
+            //then
+            assertThat(result.getContent().size()).isEqualTo(1);
+            assertThat(result.getContent().get(0).getContent()).isEqualTo(recruitment1.getContent());
+            assertThat(result.getContent().get(0).getTitle()).isEqualTo(recruitment1.getTitle());
+            assertThat(result.getContent().get(0).getTeam().getId()).isEqualTo(recruitment1.getTeam().getId());
         }
     }
 }
