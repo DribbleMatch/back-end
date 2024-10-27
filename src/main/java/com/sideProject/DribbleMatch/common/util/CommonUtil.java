4e96f5feab15236d2afd026f58a95f2a62ea7aa3
JuhyunPark <epahs1831@naver.com>
2024-10-28 00:25:10 +0900

    Fix: 회원 가입 시 농구 경력, 농구 실력 처리 안되던 현상 해결 -#79

--- src/main/java/com/sideProject/DribbleMatch/common/util/CommonUtil.java
+++ src/main/java/com/sideProject/DribbleMatch/common/util/CommonUtil.java
@@ -1,8 +1,10 @@
 package com.sideProject.DribbleMatch.common.util;
 
+import com.sideProject.DribbleMatch.entity.user.ENUM.Skill;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 @Component
 public class CommonUtil {
@@ -39,6 +41,19 @@ public class CommonUtil {
                 .replace("SG", "슈팅가드")
                 .replace("PG", "포인트가드")
                 .replace(",", " ,");
+    }
+
+    public static String createSkillString(Skill skill) {
+
+        Map<Skill, String> skillMap = Map.of(
+                Skill.BEGINNER, "초급자",
+                Skill.INTERMEDIATE, "중급자",
+                Skill.ADVANCED, "고급자",
+                Skill.FORMER_ATHLETE, "프로 선수 출신",
+                Skill.ATHLETE, "프로 선수"
+        );
+
+        return skillMap.getOrDefault(skill, "알 수 없음");
     }
 
     public static int calculateAge(LocalDate birth) {
