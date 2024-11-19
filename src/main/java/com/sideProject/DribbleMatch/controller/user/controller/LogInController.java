958770aa3d7bed31e9df4a20a60d0a94ee220a05
JuhyunPark <epahs1831@naver.com>
2024-11-20 07:06:12 +0900

    Update: 날짜 UI 수정 -#97

--- src/main/java/com/sideProject/DribbleMatch/controller/user/controller/LogInController.java
+++ src/main/java/com/sideProject/DribbleMatch/controller/user/controller/LogInController.java
@@ -6,7 +6,6 @@ import com.sideProject.DribbleMatch.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
